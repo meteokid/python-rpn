0340644dba71b822b63a0191be5ab4e5068df44a
Stephane Chamberland <armnsch@arxt20.cmc.ec.gc.ca>
2010-03-01 16:05:55 +0000

    From GEM v_4.1.1

---
+++ dyn/msg.h
@@ -0,0 +1,15 @@
+#if !defined(MSG_H)
+#define MSG_H
+
+#define MSG_DEBUG 1
+#define MSG_INFO 2
+#define MSG_WARNING 3
+#define MSG_ERROR 4
+#define MSG_CRITICAL 5
+#define MSG_NBLEVELS 5
+
+#define MSG_STDERR  0
+#define MSG_STDOUT 6
+
+#define MSG_MAXLEN 1024
+#endif
