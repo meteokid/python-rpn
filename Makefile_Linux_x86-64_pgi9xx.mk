0792c2dd9184543faa2a8a793dd7409b5450a08a
Stephane Chamberland <Stephane.Chamberland@ec.gc.ca>
2013-12-04 21:26:47 +0000

    define CCNAME CCOPT for older comp_arch (cherry picked from commit 54409c2a5556925a125eca595d5bdb9288be21f4)

--- Makefile_Linux_x86-64_pgi9xx.mk
+++ Makefile_Linux_x86-64_pgi9xx.mk
@@ -1,3 +1,4 @@
 MAKE = make
 PYARCH= linux-x86_64-2.6
-
+CCNAME = unix
+CCOPT  = -Wtrigraphs -fpic
--- rpnstd.py
+++ rpnstd.py
@@ -1312,4 +1312,6 @@ if __name__ == "__main__":
     import doctest
     doctest.testmod()
 
+# -*- Mode: C; tab-width: 4; indent-tabs-mode: nil -*-
+# vim: set expandtab ts=4 sw=4:
 # kate: space-indent on; indent-mode cstyle; indent-width 4; mixedindent off;
