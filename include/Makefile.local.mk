1a7d8bddb0a34688faeac97b91c835b4302bc415
stephane.chamberland <stephane.chamberland@ec.gc.ca>
2015-03-13 19:36:08 +0000

    mv ssm build to rde

--- include/Makefile.local.mk
+++ include/Makefile.local.mk
@@ -2,75 +2,67 @@
 ## File: $gemdyn/include/Makefile.local.mk
 ##
 
-#include $(modelutils)/include/recettes
-#include $(gemdyn)/include/build_components
-
 ## GEM model and GemDyn definitions
 
-mainntr = $(ABSPREFIX)maingemntr$(ABSPOSTFIX)_$(BASE_ARCH).Abs
-maindm  = $(ABSPREFIX)maingemdm$(ABSPOSTFIX)_$(BASE_ARCH).Abs
-
-MONBINDIR = $(PWD)
-BINDIR    = $(MONBINDIR)
-
+ifeq (,$(wildcard $(gemdyn)/VERSION))
+   $(error Not found: $(gemdyn)/VERSION)
+endif
+GEMDYN_VERSION = $(shell cat $(gemdyn)/VERSION)
 
 ## Some Shortcut/Alias to Lib Names
-GEMDYN_VERSION = 
-GEMDYNLIBS  = gemdyn$(GEMDYN_VERSION)
-GEMLIBS     = $(GEMDYNLIBS)
+#GEMDYNLIBS     = gemdyn$(GEMDYN_VERSION)
+#GEMLIBS        = $(GEMDYNLIBS)
 
-OBJECTS_gemdyn = $(OBJECTS_gemdyn_base) $(OBJECTS_gemdyn_adw)
-OBJECTS_libgemdyn_cpl_stubs = cpl_stubs.o
-OBJECTS_libgemdyn_prof_stubs = prof_stubs.o
-GEMDYN_LIBS = gemdyn_main gemdyn gemdyn_cpl_stubs gemdyn_prof_stubs
-GEMDYN_LIBS_V = $(foreach item,$(GEMDYN_LIBS),$(item)_$(GEMDYN_VERSION).a)
-GEMDYN_LIBS_FILES = $(foreach item,$(GEMDYN_LIBS),lib$(item).a)
-GEMDYN_MOD_FILES = $(foreach item,$(FORTRAN_MODULES_gemdyn),$(item).[Mm][Oo][Dd])
-GEMDYN_ABS = gem_monitor_end gem_monitor_output toc2nml gemgrid checkdmpart prgemnml split3df
-GEMDYN_ABS_FILES = $(BINDIR)/gem_monitor_output $(BINDIR)/gem_monitor_end $(BINDIR)/toc2nml $(BINDIR)/gemgrid_$(BASE_ARCH).Abs $(BINDIR)/checkdmpart_$(BASE_ARCH).Abs $(BINDIR)/gemprnml_$(BASE_ARCH).Abs $(BINDIR)/split3df_$(BASE_ARCH).Abs  
+LIBCPLPATH  = 
+#LIBCPL      = gemdyn_cpl_stubs
+LIBCPL      =
 
-GEM_ABS = gemntr gemdm
+GEMDYN_LIBS_MERGED = gemdyn_main gemdyn_base gemdyn_adw
+GEMDYN_LIBS_OTHER  = $(LIBCPL)
+GEMDYN_LIBS_ALL    = $(GEMDYN_LIBS_MERGED) $(GEMDYN_LIBS_OTHER)
+GEMDYN_LIBS        = gemdyn $(GEMDYN_LIBS_OTHER) 
+GEMDYN_LIBS_V      = gemdyn_$(GEMDYN_VERSION) $(GEMDYN_LIBS_OTHER) 
+
+GEMDYN_LIBS_ALL_FILES = $(foreach item,$(GEMDYN_LIBS_ALL),lib$(item).a)
+GEMDYN_LIBS_ALL_FILES_PLUS = libgemdyn.a $(GEMDYN_LIBS_ALL_FILES) 
+
+OBJECTS_MERGED_gemdyn = $(foreach item,$(GEMDYN_LIBS_MERGED),$(OBJECTS_$(item)))
+#OBJECTS_gemdyn_cpl_stubs      = cpl_stubs.o
+
+GEMDYN_MOD_FILES  = $(foreach item,$(FORTRAN_MODULES_gemdyn),$(item).[Mm][Oo][Dd])
+
+GEM_ABS       = gemntr gemdm
 GEM_ABS_FILES = $(BINDIR)/$(mainntr) $(BINDIR)/$(maindm)
 
-CHMLIBPATH  = 
-CHM         = $(CHM_VERSION) $(CHMLIBS)
-#CHMLIBPATH  = $(ARMNLIB)/modeles/CHM/v_$(CHM_VERSION)
-#CHM         = chm
-
-#PHY         = rpnphy_stubs
-#CLASSLIBS   = rpnphy_class
-#PHYSURFACELIBS = rpnphy_surface
-#PHYCONVECTLIBS = rpnphy_convect
-#CHMLIBS = rpnphy_chm_stubs
-PHY         = $(PHYLIBS)
-
-#PROF          = prof_stubs
-PROF          = prof_003
-#PROFLIBPATH   = $(ARMNLIB)/lib/$(BASE_ARCH)
-PROFLIBPATH   =
-
-CPL         = cpl_stubs
-#CPL        = cpl_$(CPL_VERSION)
-CPLLIBPATH = /users/dor/armn/mod/cpl/v_$(CPL_VERSION)/lib/$(EC_ARCH)
-
+GEMDYN_ABS        = gem_monitor_end gem_monitor_output toc2nml gemgrid checkdmpart prgemnml split3df $(GEM_ABS)
+GEMDYN_ABS_FILES  = $(BINDIR)/gem_monitor_output $(BINDIR)/gem_monitor_end $(BINDIR)/toc2nml $(BINDIR)/gemgrid_$(BASE_ARCH).Abs $(BINDIR)/checkdmpart_$(BASE_ARCH).Abs $(BINDIR)/gemprnml_$(BASE_ARCH).Abs $(BINDIR)/split3df_$(BASE_ARCH).Abs $(BINDIR)/$(mainntr) $(BINDIR)/$(maindm) $(GEM_ABS_FILES)
 
 ## GEM model Libpath and libs
-#LIBPATH = $(PWD) $(LIBPATH_PRE) $(LIBPATHEXTRA) $(LIBSYSPATHEXTRA) $(LIBPATHOTHER) $(LIBPATH_POST)
-LIBPATHPOST  = $(CHMLIBPATH)/lib/$(EC_ARCH) $(CHMLIBPATH)/$(EC_ARCH) $(CHMLIBPATH) $(CPLLIBPATH)/lib/$(EC_ARCH) $(CPLLIBPATH) $(PROFLIBPATH)
+MODEL3_LIBAPPL = $(GEMDYN_LIBS_V)
+MODEL3_LIBPATH = $(LIBCPLPATH)
 
-#OTHERS  = $(COMM) $(VGRID) $(UTIL) $(LLAPI) $(IBM_LD)
-#LIBAPPL = $(LIBS_PRE) $(MODELUTILSLIBS) $(OTHERS) $(LIBS_POST)
-#LIBSYS  = $(LIBSYS_PRE) $(LIBSYSEXTRA) $(LIBSYS_POST)
-LIBS_PRE = $(GEMLIBS) $(PHY) $(CLASSLIBS) $(CHM) $(PATCH) $(CPL)
 
-#LIBS     = $(LIBAPPL)
+##
+.PHONY: gem_vfiles gemdyn_vfiles
+GEM_VFILES = gem_version.inc gem_version.h
+gem_vfiles: $(GEM_VFILES)
+GEMDYN_VFILES = gemdyn_version.inc gemdyn_version.h $(GEM_VFILES)
+gemdyn_vfiles: $(GEMDYN_VFILES)
+gem_version.inc:
+	.rdemkversionfile "gem" "$(GEMDYN_VERSION)" . f
+gem_version.h:
+	.rdemkversionfile "gem" "$(GEMDYN_VERSION)" . c
+gemdyn_version.inc:
+	.rdemkversionfile "gemdyn" "$(GEMDYN_VERSION)" . f
+gemdyn_version.h:
+	.rdemkversionfile "gemdyn" "$(GEMDYN_VERSION)" . c
 
+#---- Abs targets -----------------------------------------------------
 
 ## GEM model targets (modelutils/gemdyn/rpnphy)
 .PHONY: gem gemntr gemdm gem_nompi gemntr_nompi gemdm_nompi allbin_gem allbincheck_gem
-gem: $(GEM_ABS)
 
-allbin_gem: $(GEM_ABS_FILES)
+allbin_gem: $(GEM_ABS)
 	ls -l $(GEM_ABS_FILES)
 allbincheck_gem:
 	for item in $(GEM_ABS_FILES) ; do \
@@ -78,181 +70,197 @@ allbincheck_gem:
 	done ;\
 	exit 0
 
-gem_nompi: gemntr_nompi gemdm_nompi
+gem: | gemntr gemdm
 
-gemntr_nompi:
-	$(MAKE) gemntr COMM_stubs=rpn_commstubs$(COMM_VERSION) MPI=
+mainntr = $(ABSPREFIX)maingemntr$(ABSPOSTFIX)_$(BASE_ARCH).Abs
+mainntr_rel = $(ABSPREFIX)maingemntr_REL_$(BASE_ARCH).Abs
+gemntr: | gemntr_rm $(BINDIR)/$(mainntr)
+	ls -l $(BINDIR)/$(mainntr)
+gemntr_rm:
+	rm -f $(BINDIR)/$(mainntr)
+$(BINDIR)/$(mainntr): | $(GEMDYN_VFILES)
+	export MAINSUBNAME="gemntr" ;\
+	export ATM_MODEL_NAME="$${MAINSUBNAME}" ;\
+	$(RBUILD4objMPI)
 
-gemdm_nompi:
-	$(MAKE) gemdm COMM_stubs=rpn_commstubs$(COMM_VERSION) MPI=
+maindm  = $(ABSPREFIX)maingemdm$(ABSPOSTFIX)_$(BASE_ARCH).Abs
+maindm_rel  = $(ABSPREFIX)maingemdm_REL_$(BASE_ARCH).Abs
+gemdm: | gemdm_rm $(BINDIR)/$(maindm)
+	ls -l $(BINDIR)/$(maindm)
+gemdm_rm:
+	rm -f $(BINDIR)/$(maindm)
+$(BINDIR)/$(maindm): | $(GEMDYN_VFILES)
+	export MAINSUBNAME="gemdm" ;\
+	export ATM_MODEL_NAME="$${MAINSUBNAME}" ;\
+	$(RBUILD4objMPI)
 
-$(BINDIR)/$(mainntr): gemntr
-	if [[ -r $(mainntr) ]] ; then cp $(mainntr) $@ 2>/dev/null ; fi
-gemntr:
-	export ATM_MODEL_NAME="GEMNTR" ; $(RBUILD2Oa) ;\
-	if [[ x$(BINDIR) == x ]] ; then \
-		cp $@.Abs $(PWD)/$(mainntr) 2>/dev/null || true ;\
-		chmod u+x $(PWD)/$(mainntr) ;\
-		ls -lL $(PWD)/$(mainntr) ;\
-	else \
-		cp $@.Abs $(BINDIR)/$(mainntr) 2>/dev/null || true ;\
-		chmod u+x $(BINDIR)/$(mainntr) ;\
-		ls -lL $(BINDIR)/$(mainntr) ;\
-	fi
+gem_nompi: | gemntr_nompi gemdm_nompi
 
-$(BINDIR)/$(maindm): gemdm
-	if [[ -r $(maindm) ]] ; then cp $(maindm) $@ 2>/dev/null ; fi
-gemdm:
-	export ATM_MODEL_NAME="GEMDM" ; $(RBUILD2Oa) ;\
-	if [[ x$(BINDIR) == x ]] ; then \
-		cp $@.Abs $(PWD)/$(maindm) || true ;\
-		chmod u+x $(PWD)/$(maindm) ;\
-		ls -lL $(PWD)/$(maindm) ;\
-	else \
-		cp $@.Abs $(BINDIR)/$(maindm) 2>/dev/null || true ;\
-		chmod u+x $(BINDIR)/$(maindm) ;\
-		ls -lL $(BINDIR)/$(maindm) ;\
-	fi
+gemntr_nompi: | $(GEMDYN_VFILES)
+	export MAINSUBNAME="gemntr" ;\
+	export ATM_MODEL_NAME="$${MAINSUBNAME}" ;\
+	export RBUILD_COMM_STUBS=rpn_commstubs$(COMM_VERSION) ;\
+	$(RBUILD4objNOMPI)
+
+gemdm_nompi: | $(GEMDYN_VFILES)
+	export MAINSUBNAME="gemdm" ;\
+	export ATM_MODEL_NAME="$${MAINSUBNAME}" ;\
+	export RBUILD_COMM_STUBS=rpn_commstubs$(COMM_VERSION) ;\
+	$(RBUILD4objMPI)
 
 
 ## GemDyn Targets
-.PHONY: libdyn
-libdyn: rmpo $(OBJECTS)
+
+.PHONY: prgemnml gemgrid checkdmpart split3df toc2nml monitor sometools allbin allbincheck
 
 mainprgemnml=gemprnml_$(BASE_ARCH).Abs
-.PHONY: prgemnml gemgrid checkdmpart split3df toc2nml monitor sometools allbin allbincheck
-# prgemnml: $(BINDIR)/gemprnml_$(BASE_ARCH).Abs
-# 	ls -lL $(BINDIR)/gemprnml_$(BASE_ARCH).Abs
-# prgemnml.ftn90: 
-# 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/src/main/$@ $@ 2>/dev/null || true ; fi ;\
-# 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/main/$@ $@ 2>/dev/null || true ; fi
-# $(LCLPO)/prgemnml.o: prgemnml.ftn90
-# $(BINDIR)/gemprnml_$(BASE_ARCH).Abs: $(LCLPO)/prgemnml.o
-prgemnml: 
-	export ATM_MODEL_NAME="prgemnml" ;\
-	makemodelbidon prgemnml > bidon.f90 ; $(MAKE) bidon.o ; rm -f bidon.f90 ;\
-	cd $(LCLPO) ;\
-	$(RBUILD) -obj *.o -o $(mainprgemnml) $(OMP) \
-		-libpath $(PWD) $(LIBPATH) \
-		-libappl "gemdyn_main gemdyn $(MODELUTILSLIBS) $(OTHERS)" \
-		-librmn $(RMN_VERSION) \
-		-libsys $(LIBSYS) \
-		-codebeta $(CODEBETA)  \
-		-optf "=$(LFLAGS)" ;\
-	/bin/mv $(mainprgemnml) $(BINDIR)/$(mainprgemnml) ;\
-	 /bin/rm -f $(mainprgemnml)_* bidon.o
+prgemnml: | prgemnml_rm $(BINDIR)/$(mainprgemnml)
+	ls -l $(BINDIR)/$(mainprgemnml)
+prgemnml_rm:
+	rm -f $(BINDIR)/$(mainprgemnml)
+$(BINDIR)/$(mainprgemnml): | $(GEMDYN_VFILES)
+	export MAINSUBNAME="prgemnml" ;\
+	export ATM_MODEL_NAME="$${MAINSUBNAME}" ;\
+	export RBUILD_COMM_STUBS=rpn_commstubs$(COMM_VERSION) ;\
+	$(RBUILD4objNOMPI)
 
+maingemgrid=gemgrid_$(BASE_ARCH).Abs
+gemgrid: | gemgrid_rm $(BINDIR)/$(maingemgrid)
+	ls -l $(BINDIR)/$(maingemgrid)
+gemgrid_rm:
+	rm -f $(BINDIR)/$(maingemgrid)
+$(BINDIR)/$(maingemgrid): | $(GEMDYN_VFILES)
+	export MAINSUBNAME="gemgrid" ;\
+	export ATM_MODEL_NAME="$${MAINSUBNAME}" ;\
+	export RBUILD_COMM_STUBS=rpn_commstubs$(COMM_VERSION) ;\
+	$(RBUILD4objNOMPI)
 
-# gemgrid: $(BINDIR)/gemgrid_$(BASE_ARCH).Abs
-# 	ls -lL $(BINDIR)/gemgrid_$(BASE_ARCH).Abs
-# gemgrid.ftn90: 
-# 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/src/main/$@ $@ 2>/dev/null || true ; fi ;\
-# 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/main/$@ $@ 2>/dev/null || true ; fi
-# $(LCLPO)/gemgrid.o: gemgrid.ftn90
-# $(BINDIR)/gemgrid_$(BASE_ARCH).Abs: $(LCLPO)/gemgrid.o
-gemgrid:
-	export ATM_MODEL_NAME="gemgrid" ;\
-	makemodelbidon gemgrid > bidon.f90 ; $(MAKE) bidon.o ; rm -f bidon.f90 ;\
-	cd $(LCLPO) ;\
-	$(RBUILD) -obj *.o -o gemgrid_$(BASE_ARCH).Abs $(OMP) \
-		-libpath $(PWD) $(LIBPATH) \
-		-libappl "gemdyn_main gemdyn $(MODELUTILSLIBS) $(OTHERS) rpn_commstubs$(COMM_VERSION)" \
-		-librmn $(RMN_VERSION) 
-		-libsys $(LIBSYS) \
-		-codebeta $(CODEBETA) \
-		-optf "=$(LFLAGS)" ;\
-	/bin/mv gemgrid_$(BASE_ARCH).Abs $(BINDIR)/gemgrid_$(BASE_ARCH).Abs ;\
-	/bin/rm -f $(LCLPO)/bidon.o $(LCLPO)/gemgrid.o 2>/dev/null || true 
-
-
-checkdmpart: $(BINDIR)/checkdmpart_$(BASE_ARCH).Abs
+maincheckdmpart=checkdmpart_$(BASE_ARCH).Abs
+checkdmpart: | checkdmpart_rm $(BINDIR)/$(maincheckdmpart)
 	ls -lL $(BINDIR)/checkdmpart_$(BASE_ARCH).Abs
-# checkdmpart.ftn90: 
-# 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/src/main/$@ $@ 2>/dev/null || true ; fi ;\
-# 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/main/$@ $@ 2>/dev/null || true ; fi
-# $(LCLPO)/checkdmpart.o: checkdmpart.ftn90
-$(BINDIR)/checkdmpart_$(BASE_ARCH).Abs: #$(LCLPO)/checkdmpart.o
-	export ATM_MODEL_NAME="checkdmpart" ; makemodelbidon checkdmpart > bidon.f90 ; $(MAKE) bidon.o ; rm -f bidon.f90 ;\
-	cd $(LCLPO) ;\
-	$(RBUILD) -obj *.o -o $@ $(OMP) $(MPI) \
-		-libpath $(PWD) $(LIBPATH) \
-		-libappl gemdyn_main $(LIBAPPL) \
-		-librmn $(RMN_VERSION) \
-		-libsys $(LIBSYS) \
-		-codebeta $(CODEBETA) \
-		-optf "=$(LFLAGS)"
-	/bin/rm -f $(LCLPO)/bidon.o $(LCLPO)/checkdmpart.o 2>/dev/null || true 
+checkdmpart_rm:
+	rm -f $(BINDIR)/$(maincheckdmpart)
+$(BINDIR)/$(maincheckdmpart): | $(GEMDYN_VFILES)
+	export MAINSUBNAME="checkdmpart" ;\
+	export ATM_MODEL_NAME="$${MAINSUBNAME}" ;\
+	$(RBUILD4objMPI)
+
+mainsplit3df=split3df_$(BASE_ARCH).Abs
+split3df: | split3df_rm $(BINDIR)/$(mainsplit3df)
+	ls -lL $(BINDIR)/$(mainsplit3df)
+split3df_rm:
+	rm -f $(BINDIR)/$(mainsplit3df)
+$(BINDIR)/$(mainsplit3df): | $(GEMDYN_VFILES)
+	export MAINSUBNAME="split3df" ;\
+	export ATM_MODEL_NAME="$${MAINSUBNAME}" ;\
+	$(RBUILD4objMPI)
 
 
-split3df: $(BINDIR)/split3df_$(BASE_ARCH).Abs
-	ls -lL $(BINDIR)/split3df_$(BASE_ARCH).Abs
-# split3df.cdk90: 
-# 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/src/main/$@ $@ 2>/dev/null || true ; fi ;\
-# 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/main/$@ $@ 2>/dev/null || true ; fi
-# $(LCLPO)/split3df.o: split3df.cdk90
-$(BINDIR)/split3df_$(BASE_ARCH).Abs: #$(LCLPO)/split3df.o
-	export ATM_MODEL_NAME="split3df" ; makemodelbidon split3df > bidon.f90 ; $(MAKE) bidon.o ; rm -f bidon.f90 ;\
-	cd $(LCLPO) ;\
-	$(RBUILD) -obj *.o -o $@ $(OMP) -mpi \
-		-libpath $(PWD) $(LIBPATH) \
-		-libappl gemdyn_main $(LIBAPPL) \
-		-libsys $(LIBSYS) \
-		-optf "=$(LFLAGS)"
-	/bin/rm -f $(LCLPO)/bidon.o $(LCLPO)/split3df.o 2>/dev/null || true 
-
-
-toc2nml: $(BINDIR)/toc2nml
+toc2nml: | toc2nml_rm $(BINDIR)/toc2nml
 	ls -lL $(BINDIR)/toc2nml
-# toc2nml.cdk90: 
-# 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/src/main/$@ $@ 2>/dev/null || true ; fi ;\
-# 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/main/$@ $@ 2>/dev/null || true ; fi
-# $(LCLPO)/toc2nml.o: toc2nml.cdk90
-$(BINDIR)/toc2nml: #$(LCLPO)/toc2nml.o
-	export ATM_MODEL_NAME="toc2nml" ; makemodelbidon toc2nml > bidon.f90 ; $(MAKE) bidon.o ; rm -f bidon.f90 ;\
-	cd $(LCLPO) ;\
-	$(RBUILD) -obj *.o -o $@ $(OMP) \
-		-libpath $(PWD) $(LIBPATH) \
-		-libappl "gemdyn_main $(VGRID) $(UTIL)" \
-		-librmn $(RMN_VERSION) \
-		-libsys $(LIBSYS) \
-		-codebeta $(CODEBETA) \
-		-optf "=$(LFLAGS)" 
-	/bin/rm -f $(LCLPO)/bidon.o 2>/dev/null || true 
-
-monitor: $(BINDIR)/gem_monitor_end $(BINDIR)/gem_monitor_output
+toc2nml_rm:
+	rm -f $(BINDIR)/toc2nml
+$(BINDIR)/toc2nml: | $(GEMDYN_VFILES)
+	export MAINSUBNAME="toc2nml" ;\
+	export ATM_MODEL_NAME="$${MAINSUBNAME}" ;\
+	export RBUILD_COMM_STUBS=rpn_commstubs$(COMM_VERSION) ;\
+	$(RBUILD4objNOMPI)
 
 gem_monitor_end: $(BINDIR)/gem_monitor_end
 	ls -lL $(BINDIR)/$@
 gem_monitor_end.c: 
+	if [[ ! -f $@ ]] ; then rdeco $@ || true ; fi
 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/src/main/$@ $@ || true ; fi
 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/main/$@ $@ || true ; fi
 $(BINDIR)/gem_monitor_end: gem_monitor_end.c
-	$(MAKE) gem_monitor_end.o
-	$(RBUILD) $(OMP) -obj gem_monitor_end.o -o $@ -conly
-	rm -f gem_monitor_end.o 2>/dev/null || true
+	mybidon=gem_monitor_end_123456789 ;\
+	cat gem_monitor_end.c | sed 's/main_gem_monitor_end/main/' > $${mybidon}.c ;\
+	$(RDECC) -o $@ -src $${mybidon}.c && rm -f $${mybidon}.[co]
 
 gem_monitor_output: $(BINDIR)/gem_monitor_output
 	ls -lL $(BINDIR)/$@
 gem_monitor_output.c: 
+	if [[ ! -f $@ ]] ; then rdeco $@ || true ; fi
 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/src/main/$@ $@ || true ; fi
 	if [[ ! -f $@ ]] ; then cp $(gemdyn)/main/$@ $@ || true ; fi
 $(BINDIR)/gem_monitor_output: gem_monitor_output.c
-	$(MAKE) gem_monitor_output.o
-	$(RBUILD) $(OMP) -obj gem_monitor_output.o -o $@ -conly
-	rm -f gem_monitor_output.o 2>/dev/null || true
+	mybidon=gem_monitor_output_123456789 ;\
+	cat gem_monitor_output.c | sed 's/main_gem_monitor_output/main/' > $${mybidon}.c ;\
+	$(RDECC) -o $@ -src $${mybidon}.c && rm -f $${mybidon}.[co]
 
-sometools: prgemnml gemgrid toc2nml
+monitor: | $(BINDIR)/gem_monitor_end $(BINDIR)/gem_monitor_output
+sometools: | prgemnml gemgrid toc2nml
 
-allbin_gemdyn: $(GEMDYN_ABS)
-#allbin_gemdyn:  toc2nml gemgrid checkdmpart prgemnml split3df #gemabs
-#allbin_gemdyn: monitor toc2nml gemgrid prgemnml split3df #gemabs
-
-
+allbin_gemdyn: | $(GEMDYN_ABS)
 allbincheck_gemdyn:
 	for item in $(GEMDYN_ABS_FILES) ; do \
 		if [[ ! -x $${item} ]] ; then exit 1 ; fi ;\
 	done ;\
 	exit 0
 
+#---- Lib target - automated ------------------------------------------
+
+.PHONY: gemdyn_libs
+gemdyn_libs: $(OBJECTS_gemdyn) $(GEMDYN_LIBS_ALL_FILES_PLUS) | $(GEMDYN_VFILES)
+$(foreach item,$(GEMDYN_LIBS_ALL),$(eval $(call LIB_template1,$(item),GEMDYN)))
+$(foreach item,$(GEMDYN_LIBS_ALL),$(eval $(call LIB_template2,$(item),GEMDYN)))
+
+$(LIBDIR)/libgemdyn_$(GEMDYN_VERSION).a: $(OBJECTS_gemdyn) | $(GEMDYN_VFILES)
+	rm -f $@ $@_$$$$; ar r $@_$$$$ $(OBJECTS_MERGED_gemdyn); mv $@_$$$$ $@
+$(LIBDIR)/libgemdyn.a: $(LIBDIR)/libgemdyn_$(GEMDYN_VERSION).a
+	cd $(LIBDIR) ; rm -f $@ ;\
+	ln -s libgemdyn_$(GEMDYN_VERSION).a $@
+
+#----  SSM build Support ------------------------------------------------
+
+GEMDYN_SSMALL_NAME  = gemdyn_$(GEMDYN_VERSION)_all
+GEMDYN_SSMARCH_NAME = gemdyn+$(COMP_ARCH)_$(GEMDYN_VERSION)_$(SSMARCH)
+.PHONY: gemdyn_ssm gemdyn_ssm_all.ssm rm_gemdyn_ssm_all.ssm gemdyn_ssm_all rm_gemdyn_ssm_all gemdyn_ssm_arch.ssm rm_gemdyn_ssm_arch.ssm gemdyn_ssm_arch gemdyn_ssm_arch_rm
+gemdyn_ssm: gemdyn_ssm_all.ssm gemdyn_ssm_arch.ssm
+rm_gemdyn_ssm: rm_gemdyn_ssm_all.ssm rm_gemdyn_ssm_all rm_gemdyn_ssm_arch.ssm gemdyn_ssm_arch_rm
+
+gemdyn_ssm_all.ssm: gemdyn_ssm_all rm_gemdyn_ssm_all.ssm $(SSMDEPOTDIR)/$(GEMDYN_SSMALL_NAME).ssm
+rm_gemdyn_ssm_all.ssm:
+	rm -f $(SSMDEPOTDIR)/$(GEMDYN_SSMALL_NAME).ssm
+$(SSMDEPOTDIR)/$(GEMDYN_SSMALL_NAME).ssm:
+	cd $(BUILD) ; tar czvf $@ $(basename $(notdir $@))
+	ls -l $@
+
+gemdyn_ssm_all: rm_gemdyn_ssm_all $(BUILD)/$(GEMDYN_SSMALL_NAME)
+rm_gemdyn_ssm_all:
+	rm -rf $(BUILD)/$(GEMDYN_SSMALL_NAME)
+$(BUILD)/$(GEMDYN_SSMALL_NAME):
+	rm -rf $@ ; mkdir -p $@ ; \
+	rsync -av --exclude-from=$(DIRORIG_gemdyn)/.ssm.d/exclude $(DIRORIG_gemdyn)/ $@/ ; \
+	echo "Dependencies (s.ssmuse.dot): " > $@/BUILDINFO ; \
+	cat $@/ssmusedep.bndl >> $@/BUILDINFO ; \
+	.rdemk_ssm_control gemdyn $(GEMDYN_VERSION) "all ; $(BASE_ARCH)" $@/BUILDINFO $@/DESCRIPTION > $@/.ssm.d/control
+
+gemdyn_ssm_arch.ssm: gemdyn_ssm_arch rm_gemdyn_ssm_arch.ssm $(SSMDEPOTDIR)/$(GEMDYN_SSMARCH_NAME).ssm
+rm_gemdyn_ssm_arch.ssm:
+	rm -f $(SSMDEPOTDIR)/$(GEMDYN_SSMARCH_NAME).ssm
+$(SSMDEPOTDIR)/$(GEMDYN_SSMARCH_NAME).ssm:
+	cd $(BUILD) ; tar czvf $@ $(basename $(notdir $@))
+	ls -l $@
+
+gemdyn_ssm_arch: gemdyn_ssm_arch_rm $(BUILD)/$(GEMDYN_SSMARCH_NAME)
+gemdyn_ssm_arch_rm:
+	rm -rf $(BUILD)/$(GEMDYN_SSMARCH_NAME)
+$(BUILD)/$(GEMDYN_SSMARCH_NAME):
+	mkdir -p $@/include/$(EC_ARCH) $@/lib/$(EC_ARCH) $@/bin/$(BASE_ARCH) ; \
+	cp $(GEMDYN_MOD_FILES) $@/include/$(EC_ARCH) ; \
+	rsync -av $(wildcard libgemdyn*.a libgemdyn*.a.fl libgemdyn*.so) $@/lib/$(EC_ARCH)/ ; \
+	.rdemkversionfile gemdyn $(GEMDYN_VERSION) $@/include/$(EC_ARCH) f ; \
+	.rdemkversionfile gemdyn $(GEMDYN_VERSION) $@/include/$(EC_ARCH) c ; \
+	.rdemkversionfile gemdyn $(GEMDYN_VERSION) $@/include/$(EC_ARCH) sh ; \
+	.rdemkversionfile gem $(GEMDYN_VERSION) $@/include/$(EC_ARCH) f ; \
+	.rdemkversionfile gem $(GEMDYN_VERSION) $@/include/$(EC_ARCH) c ; \
+	.rdemkversionfile gem $(GEMDYN_VERSION) $@/include/$(EC_ARCH) sh ; \
+	cd $(BINDIR) ; \
+	cp $(GEMDYN_ABS_FILES) $@/bin/$(BASE_ARCH) ; \
+	mv $@/bin/$(BASE_ARCH)/$(mainntr) $@/bin/$(BASE_ARCH)/$(mainntr_rel) ;\
+	mv $@/bin/$(BASE_ARCH)/$(maindm)  $@/bin/$(BASE_ARCH)/$(maindm_rel) ;\
+	cp -R $(DIRORIG_gemdyn)/.ssm.d $@/ ; \
+	.rdemk_ssm_control gemdyn $(GEMDYN_VERSION) "$(SSMORDARCH) ; $(SSMARCH) ; $(BASE_ARCH)" $@/BUILDINFO $@/DESCRIPTION > $@/.ssm.d/control 
 
 ## ====================================================================
