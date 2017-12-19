cad66ca454a78dccda35bc6da14759bcbe5be268
stephane.chamberland <stephane.chamberland@ec.gc.ca>
2017-12-19 16:35:45 +0000

    gemdyn must check phy_step returned status

--- src/base/itf_phy_step.F90
+++ src/base/itf_phy_step.F90
@@ -87,7 +87,7 @@
       call rpn_comm_barrier (RPN_COMM_ALLGRIDS, err)
       call timing_stop  ( 46 )
 
-!      call gem_error (err_step,'itf_phy_step','Problem with phy_step')
+      call gem_error (err_step,'itf_phy_step','Problem with phy_step')
 
       call set_num_threads ( Ptopo_nthreads_dyn, F_step_kount )
 
