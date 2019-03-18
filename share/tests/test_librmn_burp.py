2e506683fc02c44ad5ce32f3932251651e28f61e
stephane.chamberland <stephane.chamberland@ec.gc.ca>
2019-03-18 22:06:45 +0000

    rpnpy: new mrbcvt_dict_path_set(), mrbcvt_dict_get(); fail resistent table_b_bufr decoding; updated table_b_bufr to version 31

--- share/tests/test_librmn_burp.py
+++ share/tests/test_librmn_burp.py
@@ -574,6 +574,87 @@ class RpnPyLibrmnBurp(unittest.TestCase):
             rmn.burp_close(funit)
 
 
+    def testmrbcvtdictsetKnownValues(self):
+        """mrbcvt_dict_path_set should give known result with known input"""
+        rpnpypath = os.getenv('rpnpy', None)
+        mypath = os.path.join(rpnpypath, 'share', 'table_b_bufr_f')
+        rmn.mrbcvt_dict_path_set(mypath)
+        d  = rmn.mrbcvt_dict(297)
+        rmn.mrbcvt_dict_path_set()  # Reset
+        d0 = {
+            'e_error'   : 0,
+            'e_cmcid'   : 297,
+            'e_bufrid'  : 1041,
+            'e_bufrid_F': 0,
+            'e_bufrid_X': 1,
+            'e_bufrid_Y': 41,
+            'e_cvt'     : 1,
+            'e_desc'    : 'VITESSE ABS. PLATEFORME, 1ERE COMPOSANTE',
+            'e_units'   : 'M/S',
+            'e_scale'   : 5,
+            'e_bias'    : -1073741824,
+            'e_nbits'   : 31,
+            'e_multi'   : 0
+            }
+        ## print 'mrbcvt_dict',d
+        for k in d.keys():
+            self.assertEqual(d0[k], d[k],
+                             'For {0}, expected {1}, got {2}'
+                             .format(k, d0[k], d[k]))
+
+
+    def testmrbcvtdictseterror(self):
+        """mrbcvt_dict_path_set should rais an error on non existing file"""
+        hasError = True
+        try:
+            rmn.mrbcvt_dict_path_set('__no_such_file__')
+            hasError = False
+        except IOError:
+            pass
+        rmn.mrbcvt_dict_path_set()  # Reset
+        self.assertEqual(hasError, True)
+
+
+    def testmrbcvtdictraiseerror(self):
+        rpnpypath = os.getenv('rpnpy', None)
+        mypath = os.path.join(rpnpypath, 'share', 'table_b_bufr_e_err')
+        rmn.mrbcvt_dict_path_set(mypath, raiseError=False)
+        d  = rmn.mrbcvt_dict(297)
+        d0 = {
+            'e_error'   : 0,
+            'e_cmcid'   : 297,
+            'e_bufrid'  : 1041,
+            'e_bufrid_F': 0,
+            'e_bufrid_X': 1,
+            'e_bufrid_Y': 41,
+            'e_cvt'     : 1,
+            'e_desc'    : 'ABSOL. PLATFORM VELOCITY, FIRST COMPONENT',
+            'e_units'   : 'M/S',
+            'e_scale'   : 5,
+            'e_bias'    : -1073741824,
+            'e_nbits'   : 31,
+            'e_multi'   : 0
+            }
+        ## print 'mrbcvt_dict',d
+        for k in d.keys():
+            self.assertEqual(d0[k], d[k],
+                             'For {0}, expected {1}, got {2}'
+                             .format(k, d0[k], d[k]))
+        rmn.mrbcvt_dict_path_set(mypath, raiseError=True)
+        hasError = True
+        try:
+            d  = rmn.mrbcvt_dict(297)
+            hasError = False
+        except:
+            pass
+        rmn.mrbcvt_dict_path_set()  # Reset
+        self.assertEqual(hasError, True)
+
+
+    def testmrbcvtdictget(self):
+        cvt = rmn.mrbcvt_dict_get()
+
+
     ## def testmrbxtrcvtKnownValues(self):
     ##     """fnomfclos should give known result with known input"""
     ##     for mypath, itype, iunit in self.knownValues:
